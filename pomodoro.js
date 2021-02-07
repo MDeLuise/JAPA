6d047e06e9228c5024a73a66b5417a686190eb67
MDeLuise <MDeLuise@MBP.local>
2021-02-07 18:08:22 +0100

    dynamic change long break interval

---
+++ pomodoro.js
@@ -0,0 +1,19 @@
+function display(secName) {
+    $(".menu-body").css("display", "none")
+    $("#menu-sec-" + secName).css("display", "initial")
+}
+
+$(document).ready(function() {
+    $("#fake-long-interval").on('change', () => {
+        var insertedVal = $("#fake-long-interval").val()
+        if (! /^\d+$/.test(insertedVal)) {
+            $("#fake-long-interval").val(4)
+        }
+        // below not working, even if whould be the correct way...
+        //$("#real-long-interval").val(insertedVal).trigger("input")
+        vm.$data.fullCycle = parseInt($("#fake-long-interval").val())
+    })
+
+    //$("#carousel").css("width", "305px") // (61 * vm.$data.phasesDisplayed)
+    //$("#name").css("width", $("#carousel").css("width"))
+})
